0e0979ee5fff48dc8f9d038b08b382ac7472ae44
Miwa G <miwa.guhres@hotmail.com>
2024-03-18 17:14:54 +0100

    Add filter activ auctions and past auctions

---
+++ src/components/CurrentAuctions.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect} from 'react'
+import { Link } from 'react-router-dom';
+
+
+function CurrentAuctions() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://auctioneer.azurewebsites.net/auction/x1y/')
+      .then(response => response.json()) // Parse response as JSON
+      .then(data => {
+        // Filter data where EndDate is today or a future date
+        const filteredData = data.filter(i => {
+          const endDate = new Date(i.EndDate);
+          const today = new Date();
+          today.setHours(0, 0, 0, 0); // Set time to midnight for accurate comparison
+          return endDate >= today;
+        });
+        setData(filteredData);
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {data.map((item, index) => (
+       
+        <div key={index} className='Card'> 
+       <Link to={"/AuctionDetails"}>
+          <ul>
+          <b>{item.AuctionID}</b>
+          <h2>{item.Title}</h2>
+          <div><b>Starting price: </b><b>{item.StartingPrice}:-</b></div>
+          <p><b>Description: </b>{item.Description}</p>
+          
+<div>Seller: <b>{item.CreatedBy}</b></div>
+<p><b>Startdate:</b> {item.StartDate}</p>
+<p><b>Enddate: </b>{item.EndDate}</p>
+
+          {/* Render other properties here if needed */}
+          </ul></Link>
+        </div>
+       
+      ))}
+     
+    </div>
+    
+  );
+}
+
+export default CurrentAuctions;
+
+
+
+
--- src/components/PastAuctions.jsx
+++ src/components/PastAuctions.jsx
@@ -2,13 +2,22 @@ import React, { useState, useEffect} from 'react'
 import { Link } from 'react-router-dom';
 
 
-function CurrentAuctions() {
+function PastAuctions() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     fetch('https://auctioneer.azurewebsites.net/auction/x1y/')
       .then(response => response.json()) // Parse response as JSON
-      .then(data => setData(data)) // Set the data in state
+      .then(data => {
+       /* This is to filter past auctions EndDate is yesterday or before  */
+          const filteredData = data.filter(i =>{
+          const endData = new Date(i.EndDate);
+          const yesterday = new Date();
+          yesterday.setDate(yesterday.getDate() -1);
+          return endData <= yesterday;
+           });
+          setData(filteredData);// Set the filterdata in state:/
+      })
       .catch(error => {
         console.error('Error fetching data: ', error);
       });
@@ -41,7 +50,7 @@ function CurrentAuctions() {
   );
 }
 
-export default CurrentAuctions;
+export default PastAuctions;
 
 
 
