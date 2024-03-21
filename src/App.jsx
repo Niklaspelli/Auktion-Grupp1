e69efd97aee43437373b09361f576a4e71cdb60f
StorfenGIT <johber01@gmail.com>
2024-03-21 11:11:33 +0100

    ny struktur för sök

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,9 @@ import NewAuktion from './components/newAuktion';
 import AuctionHome from './components/AuctionHome';
 import AuctionPast from './components/AuctionPast';
 import AuctionDetails from './components/AuctionDetails';
-import SearchComponent from './components/SearchComponent';
+
 
 function App() {
-  
-
   return (
     <>
     <MyNav />
@@ -23,6 +21,30 @@ function App() {
     </Routes>
     </>
   )
-}
+  }
+
+  const [searchTerm, setSearchTerm]= useState('');
+
+  const handleSearch = (term) =>{
+    setSearchTerm(term);
+
+  <div>
+  <MyNav onSearch={handleSearch} />
+    <Routes>
+     <Route path='/' element={<Home/>}/>
+     <Route path='/usestete' element={<UseState_page />}/>
+     <Route path='/props' element={<Props_page/>}/>
+     <Route 
+     path='/useeffect' 
+     element={<UseEffect_Page searchTerm={searchTerm}/>}
+     />
+     <Route path='/todo' element={<TodoList_Page/>}/>
+     <Route path='/meal' element={<Meal_Page searchTerm={searchTerm}/>}/>
+     <Route path="/meal-details/:mealId" element={<MealDetails />} />
+    </Routes>
+
+  </div>
+  }
+
 
 export default App
--- src/components/nav/MyNav.jsx
+++ src/components/nav/MyNav.jsx
@@ -1,31 +1,50 @@
-import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import SearchComponent from './SearchComponent'; 
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {useState } from 'react';
 
-function MyNav() { 
+function MyNav({onSearch}) {
+ const [searchTerm, setSearchTerm]= useState('');
+
+ const handleSearch =() =>{
+  onSearch(searchTerm);
+ }
+
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
-      <Container fluid>
-        <Navbar.Brand href="/newAuktion">Min Auktion</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="/">Pågående</Nav.Link>
-            <Nav.Link href="/notcurrent">Avslutade</Nav.Link>
-            {}
+    <Navbar bg="primary" expand="lg" fixed="top" >
+      <Container>
+        <Navbar.Brand href="/">React-Exercise</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto  nav-link-custom">
+            <Nav.Link href="/"  className='nav-link-custom'>Home</Nav.Link>
+            <Nav.Link href="/usestete">Usestate</Nav.Link>
+            <Nav.Link href="/props">Props</Nav.Link>
+            <Nav.Link href="/useeffect">UseEffect</Nav.Link>
+            <NavDropdown className='nav-dropdown-cunstom' title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/todo">TodoList</NavDropdown.Item>
+              <NavDropdown.Item href="/meal">Meal</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
-          <Form className="d-flex flex-grow-1 justify-content-end">
-            {}
-            <SearchComponent data={data} />
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+              value={searchTerm}
+              onChange={(e)=> setSearchTerm(e.target.value)}
+            />
+            <Button variant="secondary"onClick={handleSearch} >Search</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
