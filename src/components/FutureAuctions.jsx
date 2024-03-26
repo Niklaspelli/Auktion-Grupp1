2485c5ba26f14e621f853fc13ff0c0ea4a84866f
Niklas Pelli <niklaspelli22@gmail.com>
2024-03-26 14:22:51 +0100

    senaste

--- src/components/FutureAuctions.jsx
+++ src/components/FutureAuctions.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react'
+import { useState, useEffect} from 'react'
 import DeleteAuction from './DeleteAuction';
 import Alert from 'react-bootstrap/Alert';
 
